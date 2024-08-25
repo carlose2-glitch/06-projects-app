0847d79b747b5dc41b4b9f47bfb9ed5daf4807ce
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-08-24 20:34:32 -0400

    estructuras de los componentes

---
+++ src/modules/projects/layout/ProjectsLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Top menu -->
+    <top-menu />
+    <main>
+      <!-- Side Menu -->
+      <side-menu />
+      <!-- RouterView -->
+
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
+import ProjectsLayout from '@/modules/projects/layout/ProjectsLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout,
+    },
+  ],
 });
 
 export default router;

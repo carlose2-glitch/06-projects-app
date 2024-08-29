d312585efcf4554af5bc3f8f59ae29a86bdc2e6c
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-08-28 21:49:39 -0400

    estructura de projects view

---
+++ src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
+        <tr>
+          <th>1</th>
+          <td>Cy Ganderton</td>
+          <td>Quality Control Specialist</td>
+          <td>Blue</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <button class="btn btn-circle btn-secondary fixed bottom-0 right-10">
+      <svg
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="#000000"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="miter"
+      >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+          <circle cx="12" cy="12" r="10"></circle>
+          <line x1="12" y1="8" x2="12" y2="16"></line>
+          <line x1="16" y1="12" x2="8" y2="12"></line>
+        </g>
+      </svg>
+    </button>
+  </div>
+</template>

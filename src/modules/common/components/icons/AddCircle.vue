dd73a60062ce3a132736a2ffe16995472484a687
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-08-28 22:22:04 -0400

    boton flotante con slots

---
+++ src/modules/common/components/icons/AddCircle.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="#000000"
+    stroke-width="1"
+    stroke-linecap="round"
+    stroke-linejoin="miter"
+  >
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <circle cx="12" cy="12" r="10"></circle>
+      <line x1="12" y1="8" x2="12" y2="16"></line>
+      <line x1="16" y1="12" x2="8" y2="12"></line>
+    </g>
+  </svg>
+</template>
--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -20,25 +20,12 @@
         </tr>
       </tbody>
     </table>
-
-    <button class="btn btn-circle btn-secondary fixed bottom-0 right-10">
-      <svg
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        stroke="#000000"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="miter"
-      >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-        <g id="SVGRepo_iconCarrier">
-          <circle cx="12" cy="12" r="10"></circle>
-          <line x1="12" y1="8" x2="12" y2="16"></line>
-          <line x1="16" y1="12" x2="8" y2="12"></line>
-        </g>
-      </svg>
-    </button>
   </div>
+
+  <fab-button @click="() => console.log('click')"> <AddCircle /> </fab-button>
 </template>
+
+<script lang="ts" setup>
+import FabButton from '@/modules/common/components/FabButton.vue';
+import AddCircle from '@/modules/common/components/icons/AddCircle.vue';
+</script>

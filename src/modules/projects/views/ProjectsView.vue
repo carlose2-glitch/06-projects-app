952d0048cfea202bf4c11c6c39be077de61cc8fe
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-09-03 17:24:25 -0400

    modals sin slots

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -22,10 +22,13 @@
     </table>
   </div>
 
+  <InputModal />
+
   <fab-button @click="() => console.log('click')"> <AddCircle /> </fab-button>
 </template>
 
 <script lang="ts" setup>
 import FabButton from '@/modules/common/components/FabButton.vue';
 import AddCircle from '@/modules/common/components/icons/AddCircle.vue';
+import InputModal from '@/modules/common/components/inputModal.vue';
 </script>

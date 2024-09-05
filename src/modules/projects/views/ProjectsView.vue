ada4e0864c114091e4a24dcd89e94ee27725ea1b
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-09-05 17:49:35 -0400

    funcionalidad del modal

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -22,13 +22,27 @@
     </table>
   </div>
 
-  <InputModal />
+  <InputModal
+    :open="modalOpen"
+    @close="modalOpen = false"
+    @value="onNewValue"
+    placeholder="Ingrese el nombre del proyecto"
+    title="Nuevo proyecto"
+    sub-title="Dale un nombre unico a tu proyecto"
+  />
 
-  <fab-button @click="() => console.log('click')"> <AddCircle /> </fab-button>
+  <fab-button @click="modalOpen = true"> <AddCircle /> </fab-button>
 </template>
 
 <script lang="ts" setup>
 import FabButton from '@/modules/common/components/FabButton.vue';
 import AddCircle from '@/modules/common/components/icons/AddCircle.vue';
 import InputModal from '@/modules/common/components/inputModal.vue';
+import { ref } from 'vue';
+
+const modalOpen = ref(false);
+
+const onNewValue = (projectName: String) => {
+  console.log({ projectName });
+};
 </script>

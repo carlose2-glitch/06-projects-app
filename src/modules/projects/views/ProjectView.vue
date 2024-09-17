496b7dc7df5de8d5bb2b313434df23fa42815b22
carlose2-glitch <carloseduardosanchezcarrasco@gmail.com>
2024-09-17 12:26:03 -0400

    redireccionar si no existe el proyecto

--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -1,5 +1,43 @@
 <template>
-  <bread-crumbs :name="project?.name ?? 'No name'"></bread-crumbs>
+  <div class="w-full">
+    <section class="m-2">
+      <bread-crumbs :name="project?.name ?? 'No name'"></bread-crumbs>
+    </section>
+
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support</td>
+            </tr>
+
+            <tr class="hover">
+              <th>2</th>
+              <td>
+                <input
+                  type="text"
+                  class="input input-primary w-full opacity-60 transition-all hover:opacity-100 focus:opacity-100"
+                  placeholder="Nueva tarea"
+                />
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,10 +45,12 @@ import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectsStore } from '../stores/projects.store';
 import { ref, watch } from 'vue';
 import type { Project } from '../interfaces/project.interface';
+import { useRouter } from 'vue-router';
 interface Props {
   id: String;
 }
 
+const router = useRouter();
 const props = defineProps<Props>();
 const projectStore = useProjectsStore();
 
@@ -20,6 +60,9 @@ watch(
   () => props.id,
   () => {
     project.value = projectStore.projectList.find((project) => project.id === props.id);
+    if (!project.value) {
+      router.replace('/');
+    }
   },
   {
     immediate: true,
